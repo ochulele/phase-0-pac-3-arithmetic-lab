111458bfc289740c0d93d4336a646e70e4d20920
ochuk <ochuciano@gmail.com>
2023-02-28 22:48:51 -0500

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+    return a + b;
+};
+
+function subtract() {
+    return a - b;
+};
+
+function multiply() {
+    return a*b;
+};
+
+function divide() {
+    return a/b;
+};
+
+function add(a, b) {
+    return (a + b);
+};
+
+function subtract(a, b) {
+    return (a - b);
+};
+
+function multiply(a, b) {
+    return (a * b);
+};
+
+function divide(a, b) {
+    return (a/b);
+};
+
+function increment(a) {
+    return (a + 1);
+};
+
+function decrement(a) {
+    return (a - 1);
+};
+
+
+function makeInt(a) {
+    return parseInt(a, 10);
+};
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+};
